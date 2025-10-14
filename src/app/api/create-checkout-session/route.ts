c17c4d15fc935a10c0023808e9b185f93010a520
ccle0008 <ccle0008@student.monash.edu>
2025-10-14 15:21:08 +1100

    Fixing vercel deployment.

--- src/app/api/create-checkout-session/route.ts
+++ src/app/api/create-checkout-session/route.ts
@@ -1,9 +1,20 @@
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
-const stripe = new Stripe(process.env.STRIPE_LIVE_SECRET_KEY || process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: "2025-08-27.basil",
-});
+// const stripe = new Stripe(process.env.STRIPE_LIVE_SECRET_KEY || process.env.STRIPE_SECRET_KEY!, {
+//   apiVersion: "2025-08-27.basil",
+// });
+
+const isProd = process.env.NODE_ENV === "production";
+
+const stripe = new Stripe(
+  isProd
+    ? process.env.STRIPE_LIVE_SECRET_KEY!
+    : process.env.STRIPE_SECRET_KEY!,
+  {
+    apiVersion: "2025-08-27.basil",
+  }
+);
 
 const DOMAIN = process.env.NEXT_PUBLIC_DOMAIN ?? "http://localhost:3000";
 
