46911f6d61ed073feffed34bf7f72b02d1ed5fdb
ccle0008 <ccle0008@student.monash.edu>
2025-11-27 16:34:41 +1100

    Adding sign up for company.

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -6,7 +6,7 @@ import { Suspense } from "react";
 export default function SignupPage() {
   return (
     <Suspense fallback={<div>Loading Signup...</div>}>
-  <Signup />;
+      <Signup />
     </Suspense>
   );
 }
